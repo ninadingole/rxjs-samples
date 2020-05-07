77fa23893d0159faa970a392e106234572294707
Ninad Ingole <needna78@gmail.com>
2020-05-07 09:34:55 +0530

    sample for error and complete

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {Observable} from 'rxjs';
 import { allBooks } from './data';
+import { setTimeout } from 'timers';
 
 function subscribe(subscriber) {
     for(let book of allBooks) {
@@ -9,4 +10,30 @@ function subscribe(subscriber) {
 
 let allBooksObservable$ = new Observable(subscribe);
 
-allBooksObservable$.subscribe(book => console.log(book));
+// allBooksObservable$.subscribe(book => console.log(book));
+
+
+// emit error and complete the stream
+// to run this sample change title in index.html to RxJsSamples
+
+function subscribe1(subscriber) {
+
+    if (document.title !== 'RxJsSamples') {
+        subscriber.error('Not a valid rx js sample page');
+    }
+
+    for(let book of allBooks) {
+        subscriber.next(book);
+    }
+
+    setTimeout(() => {
+        subscriber.complete();
+    }, 2000);
+
+    return () => console.log('teadown execution');
+}
+
+let allBooksObservable1$ = new Observable(subscribe1);
+// allBooksObservable1$.subscribe(book => console.log(book.title));
+
+
