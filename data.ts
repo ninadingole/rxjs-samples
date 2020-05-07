511cae16442e1ff980391f49659c8b32e3a9ee91
Ninad Ingole <needna78@gmail.com>
2020-05-07 09:29:02 +0530

    basic rxjs sample

---
+++ data.ts
@@ -0,0 +1,15 @@
+
+export const allReaders = [
+    {readerID: 1, name: 'Martin', weeklyReadingGoal: 400, totalMinutesRead: 6600},
+    {readerID: 2, name: 'Andrew', weeklyReadingGoal: 200, totalMinutesRead: 3000},
+    {readerID: 3, name: 'John', weeklyReadingGoal: 300, totalMinutesRead: 600},
+]
+
+export const allBooks = [
+    {bookID: 1, title: 'Red, White & Royal Blue', publicationYear: '2019', author: 'Casey McQuiston', genre: 'romance'},
+    {bookID: 2, title: 'The Institute', publicationYear: '2019', author: 'Stephen King', genre: 'horror'},
+    {bookID: 3, title: 'Dear Girls: Intimate Tales, Untold Secrets, & Advice for Living Your Best Life', publicationYear: '2019', author: 'Ali Wong', genre: 'humor'},
+    {bookID: 4, title: 'Over the Top: A Raw Journey to Self-Love', publicationYear: '2019', author: 'Jonathan Van Ness', genre: 'autobiography'},
+    {bookID: 5, title: 'Girl, Stop Apologizing: A Shame-Free Plan for Embracing and Achieving Your Goals', publicationYear: '2019', author: 'Rachel Hollis', genre: 'non-ficton'},
+    {bookID: 6, title: 'Will My Cat Eat My Eyeballs? Big Questions from Tiny Mortals About Death', publicationYear: '2019', author: 'Caitlin Doughty, Dianné Ruz', genre: 'sci-fi'},
+]
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-alert('hello world 1');
+import {Observable} from 'rxjs';
+import { allBooks } from './data';
+
+function subscribe(subscriber) {
+    for(let book of allBooks) {
+        subscriber.next(book);
+    }
+}
+
+let allBooksObservable$ = new Observable(subscribe);
+
+allBooksObservable$.subscribe(book => console.log(book));
